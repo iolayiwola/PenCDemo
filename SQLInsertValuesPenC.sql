73b5d6762817931cb1e956cd8b377f46cc852490
Ibrahim Olayiwola <ola.ia1523@gmail.com>
2022-03-28 18:24:47 +0100

    Add files via upload
    Inserting values in pen c Tables.

---
+++ SQLInsertValuesPenC.sql
@@ -0,0 +1,28 @@
+-- Insert values into created tables.
+-- AdminDemographics
+INSERT INTO AdminDemographics VALUES
+(1001, 'Adebayo', 'Hassan', 'Male', 234705, 'Director'),
+(1011, 'Tolulope', 'Oluwafuyi', 'Female', 234801, 'Assistant Director'),
+(1021, 'Talatu', 'Abdullah', 'Female', 234802, 'Farm Manager'),
+(1031, 'Malcolm', 'Nwosu', 'Male', 2348021, 'Financial Accountant'),
+(1041, 'Stanley', 'Edemuren', 'Male', 23470811, 'Farm Secetary')
+
+--CustomerDetails
+INSERT INTO CustomerDetails VALUES
+(1100, 'Bowale', 'Dagunduro', 'Female', 08020111),
+(1101, 'Feyisara', 'Adewale', 'Female', 08020222),
+(1102, 'Tayo', 'Itunu', 'Female', 07050111),
+(1103, 'Bisola', 'Aromire', 'Female', 07082111),
+(1104, 'Deborah', 'Ayodeji', 'Female', 0803111)
+
+--Daily Records
+INSERT INTO DailyRecords VALUES
+(001, 'Thursday', 1, 257, 0, 0, 'Newcastle', '300', 257),
+(002, 'Friday', 1, 257, 0, 2, 'Vitamin', '20 ml', 255),
+(003, 'Saturday', 1, 255, 0, 1, 'Vitamin', '20 ml', 254),
+(004, 'Sunday', 1, 254, 0, 1, 'Vitamin', '20 ml', 253),
+(005, 'Monday', 1, 253, 0, 0, 'Null' ,'Null' , 253),
+(006, 'Tuesday', 1, 253, 0, 0,'Null' ,'Null' , 253),
+(007, 'Wednesday', 1, 253, 0, 1,'Null' ,'Null' , 252),
+(008, 'Thursday', 2, 252, 0, 0, '1st Gumboro', '300', 252),
+(009, 'Friday', 2, 252, 0, 1, 'Vitamin', '20 ml', 251)
